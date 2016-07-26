6d19f7cac9bdfce786447be29a9ef94e2694de2d
Philip Morris <phillip.morris@mergermarket.com>
2016-07-26 11:24:49 +0100

    exclude dist directory

--- noDevCode.sh
+++ noDevCode.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function checkFor {
-  if [ $(grep -n -i $2 -r . --exclude-dir=node_modules | wc -l | sed 's/\s//g') -ne '0' ]
+  if [ $(grep -n -i $2 -r . --exclude-dir=dist --exclude-dir=node_modules | wc -l | sed 's/\s//g') -ne '0' ]
   then
     echo ' *********************************************'
     echo '***********************************************'
     echo "** You screwed up! You left in a $1 **"
     echo '***********************************************'
     echo ' *********************************************'
-    grep -n -i $2 -r . --exclude-dir=node_modules
+    grep -n -i $2 -r . --exclude-dir=dist --exclude-dir=node_modules
     exit 1
   fi
 }
